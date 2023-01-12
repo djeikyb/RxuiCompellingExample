d5f7eba9638f53c720f107a93a85b4d8027f225c
Jacob Certain <jace42@gmail.com>
2023-01-11 17:50:09 -0800

    first

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,45 @@
+using System.Reactive;
+using System.Reactive.Disposables;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+
+namespace RxuiCompellingExample
+{
+    public partial class MainWindow : ReactiveWindow<AppViewModel>
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // We create our bindings here. These are the code behind bindings which allow
+            // type safety. The bindings will only become active when the Window is being shown.
+            // We register our subscription in our disposableRegistration, this will cause
+            // the binding subscription to become inactive when the Window is closed.
+            // The disposableRegistration is a CompositeDisposable which is a container of
+            // other Disposables. We use the DisposeWith() extension method which simply adds
+            // the subscription disposable to the CompositeDisposable.
+            this.WhenActivated(disposableRegistration =>
+            {
+                // Notice we don't have to provide a converter, on WPF a global converter is
+                // registered which knows how to convert a boolean into visibility.
+                this.OneWayBind(ViewModel,
+                        viewModel => viewModel.IsAvailable,
+                        view => view.searchResultsListBox.IsVisible)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel,
+                        viewModel => viewModel.SearchResults,
+                        view => view.searchResultsListBox.Items)
+                    .DisposeWith(disposableRegistration);
+
+                this.Bind(ViewModel,
+                        viewModel => viewModel.SearchTerm,
+                        view => view.searchTextBox.Text)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
---
+++ NugetDetailsView.axaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Reactive.Disposables;
+using Avalonia.Media;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+
+namespace RxuiCompellingExample
+{
+    // Second level derived class off ReactiveUserControl which contains the ViewModel property.
+    // In our MainWindow when we register the ListBox with the collection of
+    // NugetDetailsViewModels if no ItemTemplate has been declared it will search for
+    // a class derived off IViewFor<NugetDetailsViewModel> and show that for the item.
+    public partial class NugetDetailsView : ReactiveWindow<NugetDetailsViewModel>
+    {
+        public NugetDetailsView()
+        {
+            InitializeComponent();
+            this.WhenActivated(disposableRegistration =>
+            {
+                // Our 4th parameter we convert from Url into a BitmapImage.
+                // This is an easy way of doing value conversion using ReactiveUI binding.
+                this.OneWayBind<NugetDetailsViewModel, NugetDetailsView, Uri, IImage>(ViewModel,
+                    viewModel => viewModel.IconUrl,
+                    view => view.iconImage.Source,
+                    // url => url == null ? null : new BitmapImage(url))
+                    url => null)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel,
+                    viewModel => viewModel.Title,
+                    view => view.titleRun.Text)
+                    .DisposeWith(disposableRegistration);
+
+                this.OneWayBind(ViewModel,
+                    viewModel => viewModel.Description,
+                    view => view.descriptionRun.Text)
+                    .DisposeWith(disposableRegistration);
+
+                this.BindCommand(ViewModel,
+                    viewModel => viewModel.OpenPage,
+                    view => view.openButton)
+                    .DisposeWith(disposableRegistration);
+            });
+        }
+    }
+}
